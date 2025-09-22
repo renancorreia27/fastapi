c1e5068a07ff1673a1db3276bde04c2a004d8adf
Renan Machado <renanmachadocorreia27@gmail.com>
2025-09-22 17:27:25 -0300

    Descreva aqui o que foi alterado

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/") #Suporta requisições demoradas (banco de dados)
 async def root():
     return {"message": "Hello World"}
